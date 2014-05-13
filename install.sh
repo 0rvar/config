2fbbfea6a4501c850eab3e75e01109a9c88e02a5
Orvar Segerström <OrvarSegerstrom@gmail.com>
2014-05-13 18:55:59 +0200

    Added some things I forgot to install process

--- install.sh
+++ install.sh
@@ -8,22 +8,28 @@ software-properties-gtk
 echo "### Adding spotify key..."
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
 
+echo "### Adding sublime-text ppa..."
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
+
 echo "### Refreshing package lists"
 sudo apt-get update -qq
 
 echo "### Installing chrome and codecs..."
 sudo apt-get install -yqq ubuntu-restricted-extras chromium-browser chromium-codecs-ffmpeg-extra
 
-echo "### Installing spotify, quassel and skype"
-sudo apt-get install -yqq spotify-client skype quassel
+echo "### Installing sublime-text 2, spotify, quassel and skype"
+sudo apt-get install -yqq sublime-text spotify-client skype quassel
 
-echo "### Installing haskell-platform+ and xmonad"
+echo "### Installing haskell-platform+"
 sudo apt-get install -yqq haskell-platform haskell-platform-prof hlint cabal-install \
-  libghc-lens-dev libghc-lens-prof \
-  xmonad libghc-xmonad-dev libghc-xmonad-contrib-dev
+  libghc-lens-dev libghc-lens-prof
 
-echo "### Installing kupfer and indicator-multiload"
-sudo apt-get install -yqq kupfer indicator-multiload
+echo "### Installing xmonad and friends..."
+sudo apt-get install -yqq xmonad libghc-xmonad-dev libghc-xmonad-contrib-dev \
+  gnome-panel libappindicator1 kupfer indicator-multiload python-gpgme
+
+echo "Installing zsh"
+sudo apt-get install -yqq zsh 
 
 echo "### Distributing dotfiles"
 dir=$PWD
@@ -59,5 +65,21 @@ echo "ln -s $dir/.Xmodmap"
 ln -s $dir/.Xmodmap
 echo "ln -s $dir/.gitconfig"
 ln -s $dir/.gitconfig
+echo "mkdir .xmonad; ln -s $dir/xmonad.hs $PWD/.xmonad/xmonad.hs"
+mkdir .xmonad; ln -s $dir/xmonad.hs $PWD/.xmonad/xmonad.hs
+
+
 
 popd
+
+echo "### Installing oh-my-zsh and setting shell to zsh..."
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+chsh -s /bin/zsh
+
+echo "### Compiling xmonad..."
+xmonad --recompile
+
+echo "### TODO:"
+echo "$ indicator-multiload"
+echo "install dropbox"
+echo "Start sublime once, symlink sublime config, install sublime package manager, install packages (theme, sublime_packages.txt)
