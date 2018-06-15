65788d132254e230d6141b725a144ef828e92ccf
Orvar Segerström <orvarsegerstrom@gmail.com>
2018-06-15 21:33:21 +0200

    rbenv

--- init.extra.fish
+++ init.extra.fish
@@ -6,3 +6,9 @@ set -xg PATH $HOME/.dotfiles/.bin $PATH
 
 set -xg LESSOPEN "| src-hilite-lesspipe.sh %s"
 set -xg LESS " -R "
+
+function say
+    espeak "$argv" 2>/dev/null
+end
+
+status --is-interactive; and source (rbenv init -|psub)
