e9bd50d983224a37387b0fd0ac7789177eb51184
Orvar Segerström <orvar.segerstrom@miniumtech.com>
2018-09-03 09:38:19 +0200

    Sync

--- work/init.extra.fish
+++ work/init.extra.fish
@@ -9,7 +9,17 @@ set -xg PATH $HOME/.dotfiles/.bin $PATH
 set -xg PATH /usr/local/opt/findutils/libexec/gnubin $PATH
 
 # Use python installs (pipenv etc)
-set -xg PATH $HOME/Library/Python/3.6/bin $PATH 
+set -xg PATH $HOME/Library/Python/3.6/bin $PATH
+
+# ESP32
+function get_esp32
+    set -xg PATH $HOME/esp/xtensa-esp32-elf/bin $PATH
+
+    set -xg IDF_PATH $HOME/esp/esp-idf
+    set -xg PATH $IDF_PATH/components/esptool_py/esptool $PATH
+    set -xg PATH $IDF_PATH/components/espcoredump $PATH
+    set -xg PATH $IDF_PATH/components/partition_table $PATH
+end
 
 set -xg LANG en_US.UTF-8
 set -xg LC_ALL en_US.UTF-8
