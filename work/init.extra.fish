28e2ae937a7b60340af03b8275b213b0d095c056
Orvar Segerström <orvar@memory.ai>
2019-03-04 12:12:11 +0100

    New workplace

--- work/init.extra.fish
+++ work/init.extra.fish
@@ -6,36 +6,34 @@ set -xg PATH $HOME/.dotfiles/work/.bin $PATH
 set -xg PATH $HOME/.dotfiles/.bin $PATH
 
 # Use new gnu utils
-set -xg PATH /usr/local/opt/findutils/libexec/gnubin $PATH
+# set -xg PATH /usr/local/opt/findutils/libexec/gnubin $PATH
 
 # Use python installs (pipenv etc)
-set -xg PATH $HOME/Library/Python/3.6/bin $PATH
-set -xg PATH $HOME/Library/Python/2.7/bin $PATH
+# set -xg PATH $HOME/Library/Python/3.6/bin $PATH
+# set -xg PATH $HOME/Library/Python/2.7/bin $PATH
 
 # GO bin dir
-set -xg PATH $HOME/go/bin $PATH
+# set -xg PATH $HOME/go/bin $PATH
 
 # ESP32
-function get_esp32
-    set -xg PATH $HOME/esp/xtensa-esp32-elf/bin $PATH
+# function get_esp32
+#     set -xg PATH $HOME/esp/xtensa-esp32-elf/bin $PATH
 
-    set -xg IDF_PATH $HOME/esp/esp-idf
-    set -xg PATH $IDF_PATH/components/esptool_py/esptool $PATH
-    set -xg PATH $IDF_PATH/components/espcoredump $PATH
-    set -xg PATH $IDF_PATH/components/partition_table $PATH
-end
+#     set -xg IDF_PATH $HOME/esp/esp-idf
+#     set -xg PATH $IDF_PATH/components/esptool_py/esptool $PATH
+#     set -xg PATH $IDF_PATH/components/espcoredump $PATH
+#     set -xg PATH $IDF_PATH/components/partition_table $PATH
+# end
 
 # Android
-set -xg ANDROID_HOME $HOME/Library/Android/sdk/
-set -xg PATH $ANDROID_HOME/platform-tools/ $PATH
+# set -xg ANDROID_HOME $HOME/Library/Android/sdk/
+# set -xg PATH $ANDROID_HOME/platform-tools/ $PATH
 
 set -xg LANG sv_SE.UTF-8
 set -xg LC_ALL sv_SE.UTF-8
 
 set -xg LESSOPEN "| src-hilite-lesspipe.sh %s"
 set -xg LESS " -R "
-
-set -xg MINIUM_DEVELOPER_BUILD 1
 
 function kb
     echo Ctrl-f\t\tFind a file.
