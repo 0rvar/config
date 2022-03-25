c64c2a135d8c846c49a6f6072f65cfe9d5397abf
Orvar Segerström <orvarsegerstrom@gmail.com>
2022-03-25 07:38:45 +0100

    Sync

---
+++ .bin/backup-camera-uploads.fish
@@ -0,0 +1,11 @@
+#!/usr/bin/env fish
+set -l years (find . | awk -F"./" '{print $2}' | grep -Eo "^\d{4}" | sort | uniq)
+for year in $years
+  echo "Backing up ./$year* to Storage/Photos/Camera\ Uploads/$year/"
+  rsync -avzP --no-perms ./$year* hemulen:"/volume1/Storage/Photos/Camera\ Uploads/$year/"
+end
+
+echo "To clean up:"
+for year in $years
+  echo "rm $year*"
+end
