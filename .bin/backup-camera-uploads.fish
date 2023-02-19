9421dfd5d86f00f6e334ff819ef63eb6cd23dddd
Orvar Segerström <orvarsegerstrom@gmail.com>
2023-02-19 12:14:46 +0100

    Check in stuff

--- .bin/backup-camera-uploads.fish
+++ .bin/backup-camera-uploads.fish
@@ -2,7 +2,7 @@
 set -l years (find . | awk -F"./" '{print $2}' | grep -Eo "^\d{4}" | sort | uniq)
 for year in $years
   echo "Backing up ./$year* to Storage/Photos/Camera\ Uploads/$year/"
-  rsync -avzP --no-perms ./$year* hemulen:"/volume1/Storage/Photos/Camera\ Uploads/$year/"
+  rsync -vzP --itemize-changes ./$year* hemulen:"/volume1/Storage/Photos/Camera\ Uploads/$year/"
 end
 
 echo "To clean up:"
